50e35aac0d92ea45698f070d283c8ef433e7356e
Hanna Shyrko <hshyrko@scnsoft.com>
2016-09-20 17:58:45 +0300

    lesson by Alexei

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,24 +7,15 @@ public class MyFirstProgram {
       hello("user");
       hello("test");
 
-      double l = 5;
-      System.out.println("площать квадрата со стороной " + l + " = " + area(l));
+      Square s = new Square(5);
+      System.out.println("площать квадрата со стороной " + s.l + " = " + s.area());
 
-      double a = 5;
-      double b =6;
-      System.out.println("площать прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+      Rectangle r = new Rectangle(4, 6);
+      System.out.println("площать прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
-    }
-
-    public static double area(double len) {
-      return len * len;
-    }
-
-    public static double area(double a, double b) {
-      return a * b;
     }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by hshyrko on 9/20/2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square (double l){
+   this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
